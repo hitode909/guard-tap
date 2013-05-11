53fdc578feb567ea3ac8721e776787a7f5e7ba5b
hitode909 <hitode909@gmail.com>
2013-05-11 17:02:58 +0900

    implement

--- lib/guard/tap.rb
+++ lib/guard/tap.rb
@@ -1,7 +1,30 @@
 require "guard/tap/version"
 
+require 'guard'
+require 'guard/guard'
+require 'guard/watcher'
+
+require 'shellwords'
+
 module Guard
-  module Tap
-    # Your code goes here...
+  class Tap < Guard
+
+    autoload :Runner, 'guard/tap/runner'
+
+    def run_on_changes paths
+      paths.each{ |path|
+        Runner.run make_command(path), path
+      }
+    end
+
+    private
+
+    def make_command path
+      if options[:command]
+        "#{options[:command]} #{Shellwords.escape path} 2>&1"
+      else
+        "#{Shellwords.escape file} 2>&1"
+      end
+    end
   end
 end
---
+++ lib/guard/tap/runner.rb
@@ -0,0 +1,62 @@
+module Guard
+  class Tap
+    module Runner
+      class << self
+
+        def run command, title = command
+          notify "runnning: #{title}"
+
+          now_error = false
+          error_message = ''
+          flush_error = lambda {
+            next unless error_message.length > 0
+            notify_error error_message
+            error_message = ''
+          }
+
+          IO.popen(command, "r+"){ |io|
+            while line = io.gets
+              UI.info line
+              if line =~ /^ok/
+                now_error = false
+                flush_error.call
+              elsif line =~ /^\d+.{2}\d+$/
+                now_error = false
+                flush_error.call
+              elsif line =~ /^not ok/
+                flush_error.call
+                now_error = true
+              end
+              if now_error
+                error_message << line
+              end
+            end
+          }
+          flush_error.call
+
+          if $?.success?
+            notify_success "success: #{title}"
+          else
+            notify_error "failed: #{title}"
+          end
+        end
+
+        private
+
+        def notify message, args = { }
+          ::Guard::UI.info message
+          ::Guard::Notifier.notify message, args
+        end
+
+        def notify_success message
+          notify message, image: :success
+        end
+
+        def notify_error message
+          notify message, image: :failed
+        end
+
+      end
+    end
+  end
+end
